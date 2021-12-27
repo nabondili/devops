af4ca2ce88db9c0b1b9ecd49c53f45fee3ce7738
nabondili <sandeepbondili1@gmail.com>
2021-12-27 15:59:31 -0600

    adding ui

--- services/src/main/java/com/services/main/DataController.java
+++ services/src/main/java/com/services/main/DataController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DataController {
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String getData(){
        return "Sandeep";
     }
